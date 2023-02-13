bae15e11c48f984d37592b83031d12f3cefe4614
misha <m.komarov2@g.nsu.ru>
2023-02-13 10:36:03 +0000

    Update

--- problem1.c
+++ problem1.c
@@ -5,29 +5,45 @@
 
 #define MAX_SIZE 10000000
 
-double fillArr(double* arr, size_t len)
+void fillArr(double* arr, size_t len)
 {
     double step = 3.1415 * 2 / MAX_SIZE;
-    double phi = 0.0, sum = 0.0;
+    double phi = 0.0;
 
     #pragma acc parallel loop
     for(size_t i = 0; i < len; i++)
     {
         arr[i] = sin(phi);
         phi += step;
-	    sum += arr[i];
+    }
+}
+
+double sumArr(double* arr, size_t len)
+{
+    double sum = 0.0;
+
+    #pragma acc parallel loop reduction(+:sum)
+    for(size_t i = 0; i < len; i++)
+    {
+        sum += arr[i];
     }
 
     return sum;
 }
 
+
 int main()
 {
     double time_spent = 0.0;
+    //clock_t begin = clock();
+
+    double* arr = (double*)malloc(MAX_SIZE * sizeof(double));
+    #pragma acc data copy(arr[0:MAX_SIZE])
+
     clock_t begin = clock();
 
-    double* arr = (double*)calloc(MAX_SIZE, sizeof(double));
-    printf("Sum = %f\n", fillArr(arr, MAX_SIZE));
+    fillArr(arr, MAX_SIZE);
+    printf("Sum = %f\n", sumArr(arr, MAX_SIZE));
 
     clock_t end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
