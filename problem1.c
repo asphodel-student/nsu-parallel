119bc8a15f6d222bbba35eccf29ed685cc02f358
misha <m.komarov2@g.nsu.ru>
2023-02-14 09:04:21 +0000

    seminar

--- problem1.c
+++ problem1.c
@@ -7,22 +7,23 @@
 
 void fillArr(double* arr, size_t len)
 {
-    double step = 3.1415 * 2 / MAX_SIZE;
-    double phi = 0.0;
-
-    #pragma acc parallel loop
+    double step = 3.141592653589783 * 2 / MAX_SIZE;
+    
+    #pragma acc data copyin(step)
+    #pragma acc parallel loop vector vector_length(128) gang, present_or_copyin(arr)
     for(size_t i = 0; i < len; i++)
     {
-        arr[i] = sin(phi);
-        phi += step;
+        arr[i] = sin(step * i);
     }
 }
 
+//#pragma acc routine
 double sumArr(double* arr, size_t len)
 {
     double sum = 0.0;
-
-    #pragma acc parallel loop reduction(+:sum)
+    
+    #pragma acc data copy(sum)
+    #pragma acc parallel loop reduction(+:sum) present(arr)
     for(size_t i = 0; i < len; i++)
     {
         sum += arr[i];
@@ -35,19 +36,20 @@ double sumArr(double* arr, size_t len)
 int main()
 {
     double time_spent = 0.0;
-    //clock_t begin = clock();
-
+    
     double* arr = (double*)malloc(MAX_SIZE * sizeof(double));
-    #pragma acc data copy(arr[0:MAX_SIZE])
-
+    #pragma acc data create(arr[0:MAX_SIZE])
+    {
     clock_t begin = clock();
 
     fillArr(arr, MAX_SIZE);
-    printf("Sum = %f\n", sumArr(arr, MAX_SIZE));
+    printf("Sum = %0.23lf\n", sumArr(arr, MAX_SIZE));
 
     clock_t end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("The elapsed time is %f seconds\n", time_spent);
+    printf("The elapsed time is %lf seconds\n", time_spent);
 
+    free(arr);
+    }
     return 0;
 }
