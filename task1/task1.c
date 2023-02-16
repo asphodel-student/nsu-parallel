1b096f0379ac16e63254e878e914074f3ee2d3e3
misha <m.komarov2@g.nsu.ru>
2023-02-16 14:09:34 +0000

    Task1

--- task1/task1.c
+++ task1/task1.c
@@ -5,22 +5,29 @@
 
 #define MAX_SIZE 10000000
 
-void fillArr(double* arr, size_t len)
+#ifdef FLOAT_TYPE
+#define TYPE float
+#define FORMAT "%.23f"
+#else
+#define TYPE double
+#define FORMAT "%.23lf"
+#endif
+
+void fillArr(TYPE* arr, size_t len)
 {
-    double step = 3.141592653589783 * 2 / MAX_SIZE;
+    TYPE step = 3.141592653589 * 2.0 / MAX_SIZE;
     
     #pragma acc data copyin(step)
-    #pragma acc parallel loop vector vector_length(128) gang, present_or_copyin(arr)
+    #pragma acc parallel loop vector vector_length(128) gang, present(arr)
     for(size_t i = 0; i < len; i++)
     {
         arr[i] = sin(step * i);
     }
 }
 
-//#pragma acc routine
-double sumArr(double* arr, size_t len)
+TYPE sumArr(TYPE* arr, size_t len)
 {
-    double sum = 0.0;
+    TYPE sum = 0.0;
     
     #pragma acc data copy(sum)
     #pragma acc parallel loop reduction(+:sum) present(arr)
@@ -37,17 +44,18 @@ int main()
 {
     double time_spent = 0.0;
     
-    double* arr = (double*)malloc(MAX_SIZE * sizeof(double));
+    TYPE* arr = (TYPE*)malloc(MAX_SIZE * sizeof(TYPE));
     #pragma acc data create(arr[0:MAX_SIZE])
     {
     clock_t begin = clock();
 
     fillArr(arr, MAX_SIZE);
-    printf("Sum = %0.23lf\n", sumArr(arr, MAX_SIZE));
+    printf("Sum = ");
+    printf(FORMAT, sumArr(arr, MAX_SIZE));
 
     clock_t end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("The elapsed time is %lf seconds\n", time_spent);
+    printf("\nThe elapsed time is %lf seconds\n", time_spent);
 
     free(arr);
     }
