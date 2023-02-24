8e1ea025c7dfe22b931552c90447af2adf1a7684
asphodel-student <m.komarov2@g.nsu.ru>
2023-02-24 18:25:25 +0700

    Updated

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -3,6 +3,11 @@
 
 #define NUM_OF_PARAM 4
 
+#define CORNER1 10
+#define CORNER2 20
+#define CORNER3 30
+#define CORNER4 20
+
 int main(int argc, char** argv)
 {
     if(argc < NUM_OF_PARAM)
@@ -18,21 +23,28 @@ int main(int argc, char** argv)
     size_t gridSize = std::stoi(argv[2]);
     size_t numOfIter = std::stoi(argv[3]);
 
-    int** matrixA = new int*[gridSize];
+    double** matrixA = new double*[gridSize];
     for(size_t i = 0; i < gridSize; i++)
-        matrixA[i] = new int[gridSize];
+        matrixA[i] = new double[gridSize];
 
-    int** matrixB = new int*[gridSize];
+    double** matrixB = new double*[gridSize];
     for(size_t i = 0; i < gridSize; i++)
-        matrixB[i] = new int[gridSize];
+        matrixB[i] = new double[gridSize];
 
-    matrixA[0][0] = matrixB[0][0] = 10;
-    matrixA[0][gridSize - 1] = matrixB[0][gridSize - 1] = 20;
-    matrixA[gridSize - 1][0] = matrixB[gridSize - 1][0] = 30;
-    matrixA[gridSize - 1][gridSize - 1] = matrixB[gridSize - 1][gridSize - 1] = 20;
+    matrixA[0][0] = matrixB[0][0] = CORNER1;
+    matrixA[0][gridSize - 1] = matrixB[0][gridSize - 1] = CORNER2;
+    matrixA[gridSize - 1][0] = matrixB[gridSize - 1][0] = CORNER3;
+    matrixA[gridSize - 1][gridSize - 1] = matrixB[gridSize - 1][gridSize - 1] = CORNER4;
+
+    double step = 10.0 / gridSize;
+    for(size_t i = 1; i < gridSize - 1; i++)
+    {
+        matrixA[i][0] = matrixA[0][i] = CORNER1 * step * i;
+        matrixA[k][gridSize - 1] = matrixA[gridSize - 1][i] = CORNER2 * step * i;
+    }
 
     int error = 0, iter = 0;
-    while(maxError > error || iter < numOfIter)
+    while(maxError > error and iter < numOfIter)
     {
         for(size_t j = 1; j < gridSize - 1; j++)
         {
