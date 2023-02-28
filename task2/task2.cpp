7f44f3c61c40b251e4c752049d76b15da15639d7
misha <m.komarov2@g.nsu.ru>
2023-02-28 08:35:56 +0000

    Update

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -21,9 +21,16 @@ int main(int argc, char** argv)
     // Проверки  ...
 
     double minError = std::pow(10, -std::stoi(argv[1]));
-    size_t gridSize = std::stoi(argv[2]);
+
+    std::cout << "Min error " << minError << std::endl; 
+   
+    size_t gridSize = std::pow(std::stoi(argv[2]), 2);
     size_t numOfIter = std::stoi(argv[3]);
 
+    std::cout << "Grid size: " << gridSize << std::endl;
+    std::cout << "Max number of iteration: " << numOfIter << std::endl;
+
+    // Allocate two 2D arrays
     double** matrixA = new double*[gridSize];
     for(size_t i = 0; i < gridSize; i++)
         matrixA[i] = new double[gridSize];
@@ -34,32 +41,48 @@ int main(int argc, char** argv)
 
     matrixA[0][0] = matrixB[0][0] = CORNER1;
     matrixA[0][gridSize - 1] = matrixB[0][gridSize - 1] = CORNER2;
-    matrixA[gridSize - 1][0] = matrixB[gridSize - 1][0] = CORNER3;
-    matrixA[gridSize - 1][gridSize - 1] = matrixB[gridSize - 1][gridSize - 1] = CORNER4;
+    matrixA[gridSize - 1][0] = matrixB[gridSize - 1][0] = CORNER4;
+    matrixA[gridSize - 1][gridSize - 1] = matrixB[gridSize - 1][gridSize - 1] = CORNER3;
 
-    double step = 10.0 / gridSize;
+    double step1 = 1.0 * (CORNER2 - CORNER1) / gridSize, //From (0, 0) to (gridSize, 0)
+	   step2 = 1.0 * (CORNER1 - CORNER4) / gridSize, //From (0, 0) to (0, gridSize) 
+	   step3 = 1.0 * (CORNER2 - CORNER3) / gridSize, //From (gridSize, 0) to (gridSize, gridSize)
+	   step4 = 1.0 * (CORNER3 - CORNER4) / gridSize; //From (0, gridSize) to (gridSize, gridSize)
+
+    // Writing a boundary condition
     for(size_t i = 1; i < gridSize - 1; i++)
     {
-        matrixA[i][0] = matrixA[0][i] = CORNER1 * step * i;
-        matrixA[i][gridSize - 1] = matrixA[gridSize - 1][i] = CORNER2 * step * i;
+        matrixA[0][i] = step1 * i;
+	matrixA[i][0] = step2 * i;
+	matrixA[gridSize - 1][i] = step4 * i;
+        matrixA[i][gridSize - 1] =  step3 * i;
     }
 
+    //for (size_t i = 0; i < gridSize; i++)
+//	    std::cout << matrixA[0][i] << std::endl;
+
+    std::cout << "Start" << std::endl;
+    // Main algorithm
+    
     double error = 1; int iter = 0;
+
     while(minError < error and iter < numOfIter)
     {
         for(size_t j = 1; j < gridSize - 1; j++)
         {
             for(size_t i = 1; i < gridSize - 1; i++)
             {
-                matrixB[i][j] = 0.25 * (matrixA[i + 1][j] + matrixA[i - 1][j] + matrixA[i][j - 1] + matrixA[i][j + 1]);
-                error = std::max(error, matrixB[i][j] - matrixA[i][j]);
+                matrixB[i][j] = 0.25 * (matrixA[j + 1][i] + matrixA[j - 1][i] + matrixA[j][i - 1] + matrixA[j][i + 1]);
+                error = std::max(error, matrixB[j][i] - matrixA[j][i]);
             }
         }
         iter++;
+	//std::cout << "Iteration: " << iter << std::endl;
     }
 
     std::cout << iter << " " << error << std::endl;
 
+    // Free memory
     for(size_t i = 0; i < gridSize; i++)
         delete[] matrixA[i];
 
