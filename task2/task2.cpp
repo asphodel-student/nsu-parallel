558f49aec0f6e704e461e8d93a5bdd7e1c1e1942
misha <m.komarov2@g.nsu.ru>
2023-03-20 04:43:56 +0000

    Fixed kernel/update mistake

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -10,92 +10,93 @@
 
 int main(int argc, char** argv)
 {
-	const double minError = std::pow(10, -std::stoi(argv[1]));
-	const int size = std::stoi(argv[2]);
-	const int maxIter = std::stoi(argv[3]);
+    const double minError = std::pow(10, -std::stoi(argv[1]));
+    const int size = std::stoi(argv[2]);
+    const int maxIter = std::stoi(argv[3]);
 
-	std::cout << "Parameters: " << std::endl <<
+    std::cout << "Parameters: " << std::endl <<
 		"Min error: " << minError << std::endl << 
 		"Maximal number of iteration: " << maxIter << std::endl << 
 		"Grid size: " << size << std::endl;
 
-	double* matrixA = new double[size * size];
-	double* matrixB = new double[size * size];
+    double* matrixA = new double[size * size];
+    double* matrixB = new double[size * size];
+    std::memset(matrixA, 0, size * size * sizeof(double));
 
-	std::memset(matrixA, 0, size * size * sizeof(double));
-
-	matrixA[0] = CORNER1;
-	matrixA[size - 1] = CORNER2;
-	matrixA[size * size - 1] = CORNER3;
-	matrixA[size * (size - 1)] = CORNER4;
-
-
-	size_t totalSize = size * size;
-	const double step = (CORNER2 - CORNER1) / (size - 1);
-//	#pragma acc enter data copyin(matrixA[0:totalSize]) create(matrixB[0:totalSize])
-//	#pragma acc parallel loop 
-	for (int i = 1; i < size - 1; i++)
-	{	matrixA[i] = CORNER1 + i * step;
-		matrixA[i * size] = CORNER1 + i * step;
-		matrixA[(size - 1) * i] = CORNER2 + i * step;
-		matrixA[size * (size - 1) + i] = CORNER4 + i * step;
-	}
-
-	std::memcpy(matrixB, matrixA, size * size * sizeof(double));
+    matrixA[0] = CORNER1;
+    matrixA[size - 1] = CORNER2;
+    matrixA[size * size - 1] = CORNER3;
+    matrixA[size * (size - 1)] = CORNER4;
 
 
-	double error = 1.0;
-	int iter = 0;
+    size_t totalSize = size * size;
+    const double step = (CORNER2 - CORNER1) / (size - 1);
+    for (int i = 1; i < size - 1; i++)
+    {	matrixA[i] = CORNER1 + i * step;
+	    matrixA[i * size] = CORNER1 + i * step;
+	    matrixA[(size - 1) * i] = CORNER2 + i * step;
+	    matrixA[size * (size - 1) + i] = CORNER4 + i * step;
+    }
 
-	std::cout << "Start: " << std::endl;
+    std::memcpy(matrixB, matrixA, size * size * sizeof(double));
 
-	#pragma acc enter data copyin(matrixA[0:totalSize], matrixB[0:totalSize], error)
-	{
-	clock_t begin = clock();
-	while (error > minError && iter < maxIter)
-	{
-	        iter++;
 
-		if(iter % 100 == 0)
-		{
-		#pragma acc kernels async(1)
-		error = 0.0;
-		#pragma acc update device(error) async(1)
-		}
+    double error = 1.0;
+    int iter = 0;
 
-		#pragma acc data present(matrixA, matrixB, error)
-		#pragma acc parallel loop independent collapse(2) vector vector_length(256) gang num_gangs(256) reduction(max:error) async(1)
-		for (int i = 1; i < size - 1; i++)
-		{
-			for (int j = 1; j < size - 1; j++)
-			{
-				matrixB[i * size + j] = 0.25 * 
-					(matrixA[i * size + j - 1] +
-					matrixA[(i - 1) * size + j] +
-					matrixA[(i + 1) * size + j] +
-					matrixA[i * size + j + 1]);
+    std::cout << "Start: " << std::endl;
 
-				if(iter % 100 == 0)	error = fmax(error, matrixB[i * size + j] - matrixA[i * size + j]);
-			}
-		}
+    #pragma acc enter data copyin(matrixA[0:totalSize], matrixB[0:totalSize], error)
+    {
+    clock_t begin = clock();
+    while (error > minError && iter < maxIter)
+    {	    
+	    
+	iter++;
+
+
 	if(iter % 100 == 0)
 	{
-		#pragma acc update host(error) async(1)	
-		#pragma acc wait(1)
-	}
-		double* temp = matrixA;
-		matrixA = matrixB;
-		matrixB = temp;
-	}
-	clock_t end = clock();
-	std::cout << "Time: " << 1.0 * (end - begin) / CLOCKS_PER_SEC << std::endl; 
+	error = 0.0;
+	#pragma acc update device(error) async(1)
 	}
 
+	#pragma acc data present(matrixA, matrixB, error)
+	#pragma acc parallel loop independent collapse(2) vector vector_length(256) gang num_gangs(256) reduction(max:error) async(1)
+	for (int i = 1; i < size - 1; i++)
+	{
+	    for (int j = 1; j < size - 1; j++)
+	    {
+		matrixB[i * size + j] = 0.25 * 
+			(matrixA[i * size + j - 1] +
+			matrixA[(i - 1) * size + j] +
+			matrixA[(i + 1) * size + j] +
+			matrixA[i * size + j + 1]);
 
-	std::cout << "Iter: " << iter << " Error: " << error << std::endl;
+		error = fmax(error, fabs(matrixB[i * size + j] - matrixA[i * size + j]));
+	    }
+	}
+
+	if(iter % 100 == 0)
+	{
+	#pragma acc update host(error) async(1)	
+	#pragma acc wait(1)
+	}
+
+	double* temp = matrixA;
+	matrixA = matrixB;
+	matrixB = temp;
+    }
+
+    clock_t end = clock();
+    std::cout << "Time: " << 1.0 * (end - begin) / CLOCKS_PER_SEC << std::endl; 
+    }
+
+
+    std::cout << "Iter: " << iter << " Error: " << error << std::endl;
 	
-	delete[] matrixA;
-	delete[] matrixB;
+    delete[] matrixA;
+    delete[] matrixB;
 
-	return 0;
+    return 0;
 }
