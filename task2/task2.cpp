b370ce1e920c478c6b458cd55fc887bd84de7b40
misha <m.komarov2@g.nsu.ru>
2023-03-30 10:02:21 +0000

    fixed incorrect filling of the matrix

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -13,33 +13,34 @@ int main(int argc, char** argv)
     const double minError = std::pow(10, -std::stoi(argv[1]));
     const int size = std::stoi(argv[2]);
     const int maxIter = std::stoi(argv[3]);
+	const size_t totalSize = size * size;
 
     std::cout << "Parameters: " << std::endl <<
 		"Min error: " << minError << std::endl << 
 		"Maximal number of iteration: " << maxIter << std::endl << 
 		"Grid size: " << size << std::endl;
 
-    double* matrixA = new double[size * size];
-    double* matrixB = new double[size * size];
-    std::memset(matrixA, 0, size * size * sizeof(double));
+  	double* matrixA = new double[size * size];
+	double* matrixB = new double[size * size];
+	
+	std::memset(matrixA, 0, size * size * sizeof(double));
 
-    matrixA[0] = CORNER1;
-    matrixA[size - 1] = CORNER2;
-    matrixA[size * size - 1] = CORNER3;
-    matrixA[size * (size - 1)] = CORNER4;
+	// Adding the border conditions
+	matrixA[0] = CORNER1;
+	matrixA[size - 1] = CORNER2;
+	matrixA[size * size - 1] = CORNER3;
+	matrixA[size * (size - 1)] = CORNER4;
 
+	const double step = 1.0 * (CORNER2 - CORNER1) / (size - 1);
+	for (int i = 1; i < size - 1; i++)
+	{
+		matrixA[i] = CORNER1 + i * step;
+		matrixA[i * size] = CORNER1 + i * step;
+		matrixA[(size - 1) + i * size] = CORNER2 + i * step;
+		matrixA[size * (size - 1) + i] = CORNER4 + i * step;
+	}
 
-    size_t totalSize = size * size;
-    const double step = (CORNER2 - CORNER1) / (size - 1);
-    for (int i = 1; i < size - 1; i++)
-    {	matrixA[i] = CORNER1 + i * step;
-	    matrixA[i * size] = CORNER1 + i * step;
-	    matrixA[(size - 1) * i] = CORNER2 + i * step;
-	    matrixA[size * (size - 1) + i] = CORNER4 + i * step;
-    }
-
-    std::memcpy(matrixB, matrixA, size * size * sizeof(double));
-
+	std::memcpy(matrixB, matrixA, size * size * sizeof(double));
 
     double error = 1.0;
     int iter = 0;
@@ -51,10 +52,7 @@ int main(int argc, char** argv)
     clock_t begin = clock();
     while (error > minError && iter < maxIter)
     {	    
-	    
 	iter++;
-
-
 	if(iter % 100 == 0)
 	{
 	error = 0.0;
@@ -91,8 +89,7 @@ int main(int argc, char** argv)
     clock_t end = clock();
     std::cout << "Time: " << 1.0 * (end - begin) / CLOCKS_PER_SEC << std::endl; 
     }
-
-
+	
     std::cout << "Iter: " << iter << " Error: " << error << std::endl;
 	
     delete[] matrixA;
